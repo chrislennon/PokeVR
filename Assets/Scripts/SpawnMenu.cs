19661a8f355a5e902b4967af2850a9c108059014
Chris Lennon <leapyearboy@gmail.com>
2016-07-17 16:02:12 +0200

    menu and controller changes

---
+++ Assets/Scripts/SpawnMenu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnMenu : MonoBehaviour {
+
+    SteamVR_TrackedObject trackedObj;
+    public GameObject menuprefab;
+    private GameObject spawned;
+    //private Camera UICam;
+
+    // Use this for initialization
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+        {
+            //UICam = GameObject.Find("Controller UI Camera").GetComponent<Camera>();
+            spawned = GameObject.FindWithTag("Menu");
+            if (spawned)
+            {
+                Destroy(spawned);
+            }
+            SpawnMenu clone = (SpawnMenu)Instantiate(menuprefab, transform.position, transform.rotation);
+            
+            //Debug.Log(UICam);
+            //clone.GetComponent<Canvas>().worldCamera = UICam;
+            //clone.tag = "Menu";
+        }
+
+    }
+}
---
+++ Assets/SetControllerCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetControllerCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.GetComponent<Canvas>().worldCamera = GameObject.Find("Controller UI Camera").GetComponent<Camera>();
+    }
+}
